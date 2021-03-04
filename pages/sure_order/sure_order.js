0da440d43b9117ecb07efb9a7f5dbf1a94fb22af
范玉龙 <13067882143@163.com>
2021-03-04 22:18:39 +0800

    确认订单完成一部分

--- pages/sure_order/sure_order.js
+++ pages/sure_order/sure_order.js
@@ -2,11 +2,25 @@
 Page({
   data: {
     way_id:'1',   //配送方式id
+    phone:"",     //手机号
+    remark:"",    //留言
   },
   //切换配送方式
   checkWay(e){
     this.setData({
       way_id:e.target.dataset.way_id
+    })
+  },
+  //监听手机号
+  changePhone(e){
+    this.setData({
+      phone:e.detail.value
+    })
+  },
+  //监听留言
+  changeRemark(e){
+    this.setData({
+      remark:e.detail.value
     })
   }
 })
