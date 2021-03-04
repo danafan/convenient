1373cb7803990e7fb70640383fbc7528bee0eb87
范玉龙 <13067882143@163.com>
2021-03-04 17:55:56 +0800

    确认订单完成一部分

--- pages/sure_order/sure_order.js
+++ pages/sure_order/sure_order.js
@@ -1,66 +1,12 @@
 // pages/sure_order/sure_order.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    way_id:'1',   //配送方式id
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //切换配送方式
+  checkWay(e){
+    this.setData({
+      way_id:e.target.dataset.way_id
+    })
   }
 })
